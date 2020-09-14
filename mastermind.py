220233b8fa61339a213fc32022aacda3a591fa48
asiguqa <asiguqa@student.wethinkcode.co.za>
2020-09-14 13:02:40 +0200

    wtc-lms save

--- mastermind.py
+++ mastermind.py
@@ -1,12 +1,12 @@
 import random
 
-list = [0,0,0,0]
-u_input = ''
-d_c_place = 0
-d_inc_place = 0
-turns_left = 12
 
-def generate_code():
+def run_game():
+    """
+    TODO: implement Mastermind code here
+    """
+    list = [0,0,0,0]
+        
     c = 0
     while c < 4:
         digit = random.randint(1,8)
@@ -16,20 +16,16 @@ def generate_code():
         c += 1
 
     print('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')
-    print(list)
-
-def enter_code():
-    global u_input
+    
     u_input = input('Input 4 digit code: ')
+
     while True:
         if len(u_input) != 4:
             print('Please enter exactly 4 digits.')
             u_input = input('Input 4 digit code: ')
             continue
         break
-
-def check_zeros():
-    global u_input
+    
     control_var = True
     while control_var:
         for i in u_input:
@@ -41,12 +37,10 @@ def check_zeros():
                 control_var = False
                 continue
 
-def check_code():
-    global d_c_place
-    global d_inc_place 
-    global turns_left
-    global u_input
-
+    d_c_place = 0
+    d_inc_place = 0 
+    turns_left = 12
+    
     while d_c_place != 4 and turns_left != 0:
         for i in u_input:
             if int(i) in list:
@@ -58,7 +52,7 @@ def check_code():
         print('Number of correct digits not in correct place: {}'.format(d_inc_place))
         if d_c_place == 4:
             print('Congratulations! You are a codebreaker!')
-            print('The code was: {}'.format(u_input))
+            print('The code was: {}'.format(list))
         else:
             d_c_place = 0
             d_inc_place = 0
@@ -68,12 +62,6 @@ def check_code():
                 break
             u_input = input('Input 4 digit code: ')
     pass
-
-def run_game():
-    generate_code()
-    enter_code()
-    check_zeros()
-    check_code()
 
 if __name__ == "__main__":
     run_game()
