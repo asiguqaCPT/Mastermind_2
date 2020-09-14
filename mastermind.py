cc4f0d2df82685c310ac5141f9e6af883cb3a850
asiguqa <asiguqa@student.wethinkcode.co.za>
2020-09-14 12:29:41 +0200

    wtc-lms save

--- mastermind.py
+++ mastermind.py
@@ -1,82 +1,78 @@
 import random
 
-
-# TODO: Decompose into functions
-#global variables:
-code = [0,0,0,0] # creates code
-answer = '' # use to store user input answer
-correct_digits_and_position = 0 # keep track of correct digits and position
-correct_digits_only = 0 # keeps track of correct digits but off position
-correct = False # for stating user input as being correct
-turns = 0   # keeps track of the amount of turns user has left to guess code
+list = [0,0,0,0]
+u_input = ''
+d_c_place = 0
+d_inc_place = 0
+turns_left = 12
 
 def generate_code():
-    '''
-    Generates code to be guessed by the user 
-    '''
-    global code
-    for i in range(4):
-        value = random.randint(1, 8) # 8 possible digits
-        while value in code:
-            value = random.randint(1, 8)  # 8 possible digits
-        code[i] = value
+    c = 0
+    while c < 4:
+        digit = random.randint(1,8)
+        while digit in list:
+            digit = random.randint(1,8)
+        list[c] = digit
+        c += 1
+
     print('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')
+    print(list)
 
 def enter_code():
-    '''
-    Gets user input
-    '''
-    global answer 
+    global u_input
+    u_input = input('Input 4 digit code: ')
     while True:
-        answer = input("Input 4 digit code: ")
-        if len(answer) < 4 or len(answer) > 4:
-            print("Please enter exactly 4 digits.")
-        else:
-            break
-    
-def correct_digits_pos():
-    '''
-    Keeps track of number of correct digits and their position 
-    with respect to the code
-    '''
-    global correct_digits_and_position
-    global correct_digits_only
-    global code
-    global answer
-    
-    correct_digits_and_position = 0
-    correct_digits_only = 0
-    for i in range(len(answer)):
-        if code[i] == int(answer[i]):
-            correct_digits_and_position += 1
-        elif int(answer[i]) in code:
-            correct_digits_only += 1
-    print('Number of correct digits in correct place:     '+str(correct_digits_and_position))
-    print('Number of correct digits not in correct place: '+str(correct_digits_only))
+        if len(u_input) != 4:
+            print('Please enter exactly 4 digits.')
+            u_input = input('Input 4 digit code: ')
+            continue
+        break
+
+def check_zeros():
+    global u_input
+    control_var = True
+    while control_var:
+        for i in u_input:
+            if int(i) not in range(1,9):
+                print('Please enter exactly 4 digits.')
+                u_input = input('Input 4 digit code: ')
+                break
+            else:
+                control_var = False
+                continue
 
 def check_code():
-    '''
-    Check whether user guess is correct
-    '''
-    global correct
-    global turns
-    while not correct and turns < 12:
-        enter_code()
-        correct_digits_pos()
-        turns += 1
-        if correct_digits_and_position == 4:
-            correct = True
+    global d_c_place
+    global d_inc_place 
+    global turns_left
+    global u_input
+
+    while d_c_place != 4 and turns_left != 0:
+        for i in u_input:
+            if int(i) in list:
+                if u_input.index(i) == list.index(int(i)):
+                    d_c_place += 1
+                else:
+                    d_inc_place += 1
+        print('Number of correct digits in correct place:     {}'.format(d_c_place))
+        print('Number of correct digits not in correct place: {}'.format(d_inc_place))
+        if d_c_place == 4:
             print('Congratulations! You are a codebreaker!')
+            print('The code was: {}'.format(u_input))
         else:
-            print('Turns left: '+str(12 - turns)) 
-    print('The code was: '+str(code))
+            d_c_place = 0
+            d_inc_place = 0
+            turns_left -= 1
+            print('Turns left: {}'.format(turns_left))
+            if turns_left == 0:
+                break
+            u_input = input('Input 4 digit code: ')
+    pass
 
 def run_game():
-    '''
-    Starts the game
-    Calls the necessary functions
-    '''
     generate_code()
+    enter_code()
+    check_zeros()
     check_code()
 
 if __name__ == "__main__":
